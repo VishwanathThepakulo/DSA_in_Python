46a20f3970938e2bef51674a0560b258c27b6a02
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-04-08 23:20:10 +0530

    Bubble sort O(n^2)

--- timeComplexity.py
+++ timeComplexity.py
@@ -53,7 +53,7 @@ import time
 # print(f"Total execution time: {end_time - start_time:.6f} seconds")
 
 # start_time = time.perf_counter()
-big_nums = list(range(10000000))
+# big_nums = list(range(10000000))
 # x= big_nums[500]
 # end_time = time.perf_counter()
 # print(f"O(1) Access: {end_time-start_time:.8f}")
@@ -68,15 +68,164 @@ big_nums = list(range(10000000))
 # print(f"O(n) Access: {end_time-start_time:.8f}")
 
 
-start_time = time.perf_counter()
-big_nums.pop()
-end_time =  time.perf_counter()
-print(f"O(1) Access: {end_time-start_time:.8f}")
+# start_time = time.perf_counter()
+# big_nums.pop()
+# end_time =  time.perf_counter()
+# print(f"O(1) Access: {end_time-start_time:.8f}")
 
 
-print('------------------------------------------')
+# print('------------------------------------------')
 
-start_time = time.perf_counter()
-big_nums.pop(0)
-end_time =  time.perf_counter()
-print(f"O(n) Access: {end_time-start_time:.8f}")
+# start_time = time.perf_counter()
+# big_nums.pop(0)
+# end_time =  time.perf_counter()
+# print(f"O(n) Access: {end_time-start_time:.8f}")
+
+
+
+#  O(n^2) — Quadratic Example (Bubble Sort)
+
+
+# arr = list(range(2000,0,-1))
+arr = list(range(1,11))
+# print(arr)
+
+def bubble_sort(arr):
+    n = len(arr)
+    SWAP = False
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                SWAP = True
+            if not SWAP:
+                break   
+    return arr
+
+start = time.perf_counter()
+print(bubble_sort(arr))
+end = time.perf_counter()
+
+print(f"O(n^2) time taken is : {end-start:.6f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
