ec5f74bf5341bb094e6cade03d81555a75ad648a
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-04-13 23:53:06 +0530

    Recursion programs

--- recursion.py
+++ recursion.py
@@ -10,7 +10,7 @@
 # def countdown(n):
 #     if n == 0:
 #         var = "Done!"
-#         return var
+#         return var # Base Case   
 #     print(n)
 #     return countdown(n-1)
 # print(countdown(5))
@@ -21,8 +21,43 @@
 # Factorial
 
 
-def factorial(num):
-    if num == 0:
+# def factorial(num):
+#     if num == 0:
+#         return 1 # Base Case   
+#     return num*factorial(num-1)
+# print(factorial(5))
+
+
+# Fibonacci of n
+
+
+# def fibonacci(num):
+#     if num<=1:
+#         return num
+#     return fibonacci(num-1) + fibonacci(num-2)
+# print(fibonacci(10))
+
+
+# Sum of first n natural numbers
+
+# def sum_of_natural(num):
+#     if num<=1:
+#         return num
+#     return num+sum_of_natural(num-1)
+# print(sum_of_natural(5))
+
+
+# Power of a number (x^n)
+
+def pow_of_num(x,y):
+    if y==0:
         return 1
-    return num*factorial(num-1)
-print(factorial(5))
+    return x*pow_of_num(x, y-1)
+print(pow_of_num(2,3))
+
+
+
+
+
+
+
