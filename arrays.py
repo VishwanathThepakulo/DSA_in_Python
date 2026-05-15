62d9ceb4ad83b1c71cc82774440509bed6d815e3
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-05-15 23:53:30 +0530

    product of array except self

--- arrays.py
+++ arrays.py
@@ -596,14 +596,20 @@
 # print(ans)
 
 
+# Product of array except self 🔥
 
-
-
-
-
-
-
-
-
-
+arr = [2,4,6,8]
+n=len(arr)
+prefix = [1]*n
+suffix = [1]*n
+result = [1]*n
+for i in range(1,n):
+    prefix[i]=prefix[i-1]*arr[i-1]
+print(prefix)
+for i in range(n-2,-1,-1):
+    suffix[i]=suffix[i+1]*arr[i+1]
+print(suffix)
+for i in range(n):
+    result[i]=prefix[i]*suffix[i]
+print(result)
 
