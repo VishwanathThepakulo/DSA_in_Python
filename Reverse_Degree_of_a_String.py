dd31fec001ed1a8d87ed041dd4f34dfad205b99f
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-09-21 00:30:35 +0530

    reverse degree

---
+++ Reverse_Degree_of_a_String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        total = 0
+        for i, ch in enumerate(s,1):
+            reverse = 26 - (ord(ch)-ord('a'))
+            total += reverse*i
+        return total        
+        
+solution = Solution()
+print(solution.reverseDegree("hello"))
